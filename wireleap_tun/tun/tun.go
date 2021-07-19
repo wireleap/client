15f8c05ce6a06455c7d08d2d207a0e2348be06f7
Anton Pyrogovskyi <anton@wireleap.com>
2021-07-19 21:08:38 +0300

    tun: remove obsolete ReadPacketData func

--- wireleap_tun/tun/tun.go
+++ wireleap_tun/tun/tun.go
@@ -4,19 +4,14 @@ package tun
 
 import (
 	"net"
-	"time"
 
-	"github.com/google/gopacket"
 	"github.com/songgao/water"
 )
-
-const bufsize = 65535 // maximum IP packet size just to be safe
 
 // T is the type of a tun device.
 type T struct {
 	*water.Interface
 	NetIf *net.Interface
-	buf   []byte
 }
 
 // New() creates a new tun device.
@@ -34,20 +29,6 @@ func New() (s *T, err error) {
 	s = &T{
 		Interface: ifc,
 		NetIf:     nif,
-		buf:       make([]byte, bufsize),
 	}
-	return
-}
-
-// ReadPacketData() fulfills the gopacket.PacketSource interface.
-func (s *T) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err error) {
-	var n int
-	n, err = s.Read(s.buf)
-	if err != nil {
-		return
-	}
-	data = s.buf[:n]
-	ci.Timestamp = time.Now()
-	ci.CaptureLength = n
 	return
 }
