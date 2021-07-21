c1ee0d8b2179d2ec330b52c2ae6436da6e9a302d
Anton Pyrogovskyi <anton@wireleap.com>
2021-07-21 19:34:08 +0300

    tun: improve upstream connection synchronization

--- wireleap_tun/ptable/ptable.go
+++ wireleap_tun/ptable/ptable.go
@@ -21,18 +21,43 @@ type Entry struct {
 	SrcIP, DstIP     net.IP
 	SrcPort, DstPort int
 
-	sync.Mutex // this mutex guards Conn only
-	Conn       net.Conn
+	mu   sync.Mutex // for initializing conn
+	conn net.Conn
 }
 
 type T [nfamilies * nports]atomic.Value
 
 func (t *T) Get(f Family, port int) (e *Entry) {
 	v := t[int(f*nports)+port].Load()
-	if v == nil {
+	if v == nil || v.(*Entry) == nil {
 		return (*Entry)(nil)
 	}
-	return v.(*Entry)
+	e = v.(*Entry)
+	return
 }
-func (t *T) Set(f Family, port int, e *Entry) { t[int(f*nports)+port].Store(e) }
-func (t *T) Del(f Family, port int)           { t[int(f*nports)+port].Store((*Entry)(nil)) }
+
+func (t *T) Set(f Family, port int, e *Entry, init func() (net.Conn, error)) {
+	e.mu.Lock()
+	t[int(f*nports)+port].Store(e)
+	go func() {
+		defer e.mu.Unlock()
+		if c, err := init(); err == nil {
+			e.conn = c
+		} else {
+			t.Del(f, port)
+		}
+	}()
+}
+
+func (t *T) Del(f Family, port int) {
+	// remove reference
+	// it will get garbage collected after it goes out of scope
+	t[int(f*nports)+port].Store((*Entry)(nil))
+}
+
+func (e *Entry) Conn() net.Conn {
+	// wait until init unlocks the connection
+	e.mu.Lock()
+	e.mu.Unlock()
+	return e.conn
+}
