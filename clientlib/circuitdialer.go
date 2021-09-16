7a977ad7b61a3043d174153ccf661e399acaa5b4
Anton Pyrogovskyi <anton@wireleap.com>
2021-09-16 12:18:02 +0300

    adapt to use new wl dial func (w/ headers payload)

--- clientlib/circuitdialer.go
+++ clientlib/circuitdialer.go
@@ -21,7 +21,7 @@ import (
 func CircuitDialer(
 	skf func() (*servicekey.T, error),
 	circuitf func() ([]*relayentry.T, error),
-	dialf func(string, *url.URL) (net.Conn, error),
+	dialf func(net.Conn, string, *url.URL, *wlnet.Init) (net.Conn, error),
 ) func(string, string) (net.Conn, error) {
 	return func(protocol, target string) (c net.Conn, err error) {
 		sk, err := skf()
@@ -42,8 +42,21 @@ func CircuitDialer(
 				link.Addr.String(),
 				link.Pubkey.String(),
 			)
-			if i == 0 {
-				c, err = dialf("tcp", &link.Addr.URL)
+			switch i {
+			case 0:
+				continue
+			default:
+				st, err = sharetoken.New(sk, circuit[i-1].Pubkey.T())
+				if err != nil {
+					return
+				}
+				c, err = dialf(c, "tcp", &link.Addr.URL, &wlnet.Init{
+					Command:  "CONNECT",
+					Protocol: "tcp",
+					Remote:   link.Addr,
+					Token:    st,
+					Version:  &clientrelay.T.Version,
+				})
 				if err != nil {
 					// return circuit-specific error
 					err = &status.T{
@@ -53,28 +66,6 @@ func CircuitDialer(
 					}
 					return
 				}
-				continue
-			}
-			st, err = sharetoken.New(sk, circuit[i-1].Pubkey.T())
-			if err != nil {
-				return
-			}
-			init := &wlnet.Init{
-				Command:  "CONNECT",
-				Protocol: "tcp",
-				Remote:   link.Addr,
-				Token:    st,
-				Version:  &clientrelay.T.Version,
-			}
-			err = init.WriteTo(c)
-			if err != nil {
-				// return circuit-specific error
-				err = &status.T{
-					Code:   http.StatusBadGateway,
-					Desc:   err.Error(),
-					Origin: link.Pubkey.String(),
-				}
-				return
 			}
 		}
 		log.Printf("Now connecting to target: %s", target)
@@ -86,14 +77,17 @@ func CircuitDialer(
 		if err != nil {
 			return
 		}
-		init := &wlnet.Init{
+		c, err = dialf(c, "tcp", &circuit[len(circuit)-1].Addr.URL, &wlnet.Init{
 			Command:  "CONNECT",
 			Protocol: protocol,
 			Remote:   &texturl.URL{*u},
 			Token:    st,
 			Version:  &clientrelay.T.Version,
+		})
+		if err != nil {
+			return
 		}
-		c, err = &wlnet.FragReadConn{Conn: c}, init.WriteTo(c)
+		c = &wlnet.FragReadConn{Conn: c}
 		return
 	}
 }
