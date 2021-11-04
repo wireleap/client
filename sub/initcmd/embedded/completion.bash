589749df21354583fd8d8b459651884ac3f18dd7
Anton Pyrogovskyi <anton@wireleap.com>
2021-11-04 10:04:56 +0200

    embedded: unify zsh/bash completion script

--- sub/initcmd/embedded/completion.bash
+++ sub/initcmd/embedded/completion.bash
@@ -1,8 +1,9 @@
 # bashrc: source path/to/completion_darwin.bash
 # zshrc: autoload compinit && compinit && autoload bashcompinit && bashcompinit && source path/to/completion_darwin.bash
+# requires: awk sed find basename uniq (should all be included in coreutils)
 
 __wireleap_cmds() {
-    wireleap help $1 2>&1 | awk '/^  [a-z\-]/ {print $1}'
+    wireleap help "$1" 2>&1 | awk '/^  [a-z\-]/ {print $1}'
 }
 
 __wireleap_home() {
@@ -10,23 +11,24 @@ __wireleap_home() {
 }
 
 __wireleap_scripts() {
-    local wlhome="$(__wireleap_home)"
+    local wlhome
+    wlhome="$(__wireleap_home)"
     [ -d "${wlhome}/scripts" ] || return 1
-    find ${wlhome}/scripts -perm +111 -type f -exec basename '{}' ';' | uniq
+    find "${wlhome}/scripts" -perm -100 -type f -exec basename '{}' ';' | uniq
 }
 
 __wireleap_relays() {
-    local wlhome="$(__wireleap_home)"
+    local wlhome
+    wlhome="$(__wireleap_home)"
     [ -f "${wlhome}/relays.json" ] || return 1
-    sed -n 's/^.*\"address\": \"\(.*\)\",$/\1/p' < ${wlhome}/relays.json
+    sed -n 's/^.*\"address\": \"\(.*\)\",$/\1/p' < "${wlhome}/relays.json"
 }
 
 __wireleap_comp() {
     local words
     case "${#COMP_WORDS[@]}" in
         1)
-            words="$(__wireleap_cmds)";
-            ;;
+            words="$(__wireleap_cmds)";;
         *)
             case "${COMP_WORDS[1]}" in
                 exec) words="$(__wireleap_scripts)";;
@@ -35,13 +37,18 @@ __wireleap_comp() {
                         circuit.whitelist)
                             local cur="${COMP_WORDS[COMP_CWORD]}"
                             words="$(__wireleap_relays)"
-                            COMPREPLY=($(compgen -W "$words" -- "$cur"))
+                            # this is a hack but another way was not to be found
+                            if [ "$(basename $SHELL)" = 'zsh' ]; then
+                                COMPREPLY=($(compgen -W "$words" -- "$cur"))
+                            elif [ "$(basename $SHELL)" = 'bash' ]; then
+                                COMPREPLY=($(compgen -P \" -S \" -W "$words" -- "$cur"))
+                            fi
                             return 0
                             ;;
-                        *) words="$(__wireleap_cmds ${COMP_WORDS[1]})";;
+                        *) words="$(__wireleap_cmds "${COMP_WORDS[1]}")";;
                     esac
                     ;;
-                *) words="$(__wireleap_cmds ${COMP_WORDS[1]})";;
+                *) words="$(__wireleap_cmds "${COMP_WORDS[1]}")";;
             esac
             ;;
     esac
