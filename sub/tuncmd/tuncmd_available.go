103fbd1ae7cf845897d8c5c8bb6e46f088468fe7
Anton Pyrogovskyi <anton@wireleap.com>
2021-11-22 09:26:33 +0200

    windows: mark tuncmd as unvailable

---
+++ sub/tuncmd/tuncmd_available.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2021 Wireleap
+
+package tuncmd
+
+var Available = Cmd() == nil
---
+++ sub/tuncmd/tuncmd_windows.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Wireleap
+
+package tuncmd
+
+import (
+	"github.com/wireleap/common/cli"
+)
+
+func Cmd() *cli.Subcmd {
+	// not implemented yet
+	return nil
+}
