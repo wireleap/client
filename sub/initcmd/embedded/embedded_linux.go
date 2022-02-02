1a39e88302dec6c2d8e5fb561a737214b7613b0f
Anton Pyrogovskyi <anton@wireleap.com>
2022-02-02 21:14:19 +0200

    embed wireleap_socks

--- sub/initcmd/embedded/embedded_linux.go
+++ sub/initcmd/embedded/embedded_linux.go
@@ -4,5 +4,5 @@ package embedded
 
 import "embed"
 
-//go:embed wireleap_intercept.so wireleap_tun scripts_linux LICENSE completion.bash
+//go:embed wireleap_intercept.so wireleap_tun wireleap_socks scripts_linux LICENSE completion.bash
 var FS embed.FS
