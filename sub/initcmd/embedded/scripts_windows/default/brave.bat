c08f066f522860405e5b2dfb3d7d30671324dc1b
Anton Pyrogovskyi <anton@wireleap.com>
2022-04-16 09:54:46 +0200

    embedded: add brave exec script

---
+++ sub/initcmd/embedded/scripts_windows/default/brave.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set bin64=%ProgramFiles%\BraveSoftware\brave.exe
+set bin32=%ProgramFiles(x86)%\BraveSoftware\brave.exe
+
+set bin="%bin64%"
+if not exist "%bin64%" (
+    if not exist "%bin32%" (
+        echo "The executable file [%bin32%] does not exist."
+        exit /b 1
+    )
+    set bin="%bin32%"
+)
+
+%bin% --host-resolver-rules="MAP * ~NOTFOUND, EXCLUDE 127.0.0.1" --proxy-server="socks5://%WIRELEAP_SOCKS%" --user-data-dir="%LOCALAPPDATA%\BraveSoftware\brave-wireleap" --incognito %*
