7b329e0eb446383f68d0d1b7fe2c4a1cc32bcab9
Anton Pyrogovskyi <anton@wireleap.com>
2022-05-25 16:25:51 +0200

    reorganize restapi using mux

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -8,42 +8,48 @@ import (
 	"net/http"
 
 	"github.com/wireleap/client/broker"
+	"github.com/wireleap/common/api/provide"
 	"github.com/wireleap/common/api/status"
 )
 
 // api server stub
 type T struct {
-	br *broker.T
-	l  *log.Logger
+	br  *broker.T
+	l   *log.Logger
+	mux *http.ServeMux
 }
 
-func New(br *broker.T, l *log.Logger) *T {
-	return &T{br: br, l: l}
+func New(br *broker.T, l *log.Logger) (t *T) {
+	t = &T{br: br, l: l, mux: http.NewServeMux()}
+	t.mux.Handle("/version", provide.MethodGate(provide.Routes{
+		http.MethodGet: t.replyHandler(Version{VERSION}),
+	}))
+	t.mux.Handle("/config", provide.MethodGate(provide.Routes{
+		http.MethodGet: t.replyHandler(t.br.Config()),
+	}))
+	t.mux.Handle("/runtime", provide.MethodGate(provide.Routes{
+		http.MethodGet: t.replyHandler(RuntimeReply),
+	}))
+	t.mux.Handle("/contract", provide.MethodGate(provide.Routes{
+		http.MethodGet: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ci, err := t.br.ContractInfo()
+			if err != nil {
+				t.l.Printf("could not obtain contract info: %s", err)
+				status.ErrInternal.WriteTo(w)
+				return
+			}
+			t.reply(w, ci)
+		}),
+	}))
+	t.mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.l.Printf("%s just served %+v", r.URL.Path, r)
+		http.NotFound(w, r)
+	}))
+	return
 }
 
 func (t *T) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/version":
-		t.reply(w, Version{VERSION})
-	case "/runtime":
-		t.reply(w, RuntimeReply)
-	case "/contract":
-		ci, err := t.br.ContractInfo()
-		if err != nil {
-			t.l.Printf("could not obtain contract info: %s", err)
-			status.ErrInternal.WriteTo(w)
-			return
-		}
-		t.reply(w, ci)
-	case "/config":
-		t.reply(w, t.br.Config())
-	case "/", "":
-		w.Write([]byte("hello world"))
-		t.l.Printf("just served %+v", r)
-	default:
-		t.l.Printf("%s just served %+v", r.URL.Path, r)
-		http.NotFound(w, r)
-	}
+	t.mux.ServeHTTP(w, r)
 }
 
 func (t *T) reply(w http.ResponseWriter, x interface{}) {
@@ -54,4 +60,10 @@ func (t *T) reply(w http.ResponseWriter, x interface{}) {
 		return
 	}
 	w.Write(b)
+}
+
+func (t *T) replyHandler(x interface{}) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.reply(w, x)
+	})
 }
