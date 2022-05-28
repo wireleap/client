71c005062fb057ce29424b55d21012e9b9facc69
Anton Pyrogovskyi <anton@wireleap.com>
2022-05-28 20:33:30 +0200

    implement accesskey manager

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -4,12 +4,15 @@ package restapi
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/wireleap/client/broker"
 	"github.com/wireleap/common/api/provide"
 	"github.com/wireleap/common/api/status"
+	"github.com/wireleap/common/api/texturl"
 )
 
 // api server stub
@@ -41,10 +44,77 @@ func New(br *broker.T, l *log.Logger) (t *T) {
 			t.reply(w, ci)
 		}),
 	}))
+	t.mux.Handle("/accesskeys", provide.MethodGate(provide.Routes{
+		http.MethodGet: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.reply(w, t.newAccesskeysReply())
+		}),
+	}))
+	t.mux.Handle("/accesskeys/import", provide.MethodGate(provide.Routes{
+		http.MethodPost: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b, err := io.ReadAll(r.Body)
+			if err != nil {
+				t.l.Printf("error when reading accesskeys import request body: %s", err)
+				status.ErrRequest.WriteTo(w)
+				return
+			}
+			air := accesskeyImportRequest{}
+			if err = json.Unmarshal(b, &air); err != nil || air.URL == nil {
+				t.l.Printf("error when unmarshaling accesskeys import request: %s", err)
+				status.ErrRequest.WriteTo(w)
+				return
+			}
+			if err = t.br.AKM.Import(air.URL.String()); err != nil {
+				t.l.Printf("error when importing accesskeys: %s", err)
+				status.ErrRequest.WriteTo(w)
+				return
+			}
+		}),
+	}))
+	// catch-all handler for unrouted paths
 	t.mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t.l.Printf("%s just served %+v", r.URL.Path, r)
 		http.NotFound(w, r)
 	}))
+	return
+}
+
+type accesskeyImportRequest struct {
+	URL *texturl.URL `json:"url"`
+}
+
+type accesskeyReply struct {
+	Contract   *texturl.URL `json:"contract"`
+	Duration   int64        `json:"duration"`
+	State      string       `json:"state"`
+	Expiration int64        `json:"expiration"`
+}
+
+func (t *T) newAccesskeysReply() (rs []*accesskeyReply) {
+	ci, err := t.br.ContractInfo()
+	if err != nil {
+		return
+	}
+	// get contract info
+	if sk := t.br.AKM.CurrentSK(); sk != nil {
+		state := "active"
+		if sk.IsExpiredAt(time.Now().Unix()) {
+			state = "expired"
+		}
+		rs = append(rs, &accesskeyReply{
+			Contract:   ci.Endpoint,
+			Duration:   int64(ci.Servicekey.Duration),
+			State:      state,
+			Expiration: sk.Contract.SettlementOpen,
+		})
+	}
+	for _, p := range t.br.AKM.CurrentPofs() {
+		rs = append(rs, &accesskeyReply{
+			Contract:   ci.Endpoint,
+			Duration:   int64(ci.Servicekey.Duration),
+			State:      "inactive",
+			Expiration: p.Expiration,
+		})
+	}
 	return
 }
 
--- sub/importcmd/importcmd.go
+++ sub/importcmd/importcmd.go
@@ -3,55 +3,12 @@
 package importcmd
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strings"
 	"text/tabwriter"
-	"time"
 
-	"github.com/blang/semver"
-	"github.com/wireleap/client/clientcfg"
-	"github.com/wireleap/client/clientlib"
-	"github.com/wireleap/client/filenames"
-	"github.com/wireleap/client/version"
-	"github.com/wireleap/common/api/accesskey"
-	"github.com/wireleap/common/api/client"
-	"github.com/wireleap/common/api/consume"
-	"github.com/wireleap/common/api/interfaces/clientcontract"
-	"github.com/wireleap/common/api/pof"
 	"github.com/wireleap/common/cli"
 	"github.com/wireleap/common/cli/fsdir"
-	"github.com/wireleap/common/cli/process"
 )
-
-func download(url string) ([]byte, error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf(
-			"%s download request returned code %d: %s",
-			url, res.StatusCode, res.Status,
-		)
-	}
-	log.Printf("Downloading %s...", url)
-	return io.ReadAll(res.Body)
-}
 
 func Cmd() *cli.Subcmd {
 	fs := flag.NewFlagSet("import", flag.ExitOnError)
@@ -68,156 +25,158 @@ func Cmd() *cli.Subcmd {
 	}
 	r.Writer = tabwriter.NewWriter(r.FlagSet.Output(), 0, 8, 8, ' ', 0)
 	r.Run = func(fm fsdir.T) {
-		c := clientcfg.Defaults()
-		err := fm.Get(&c, filenames.Config)
+		/*
+			c := clientcfg.Defaults()
+			err := fm.Get(&c, filenames.Config)
 
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if fs.NArg() != 1 {
-			r.Usage()
-			os.Exit(1)
-		}
-
-		akfile := fs.Arg(0)
-		data := []byte{}
-
-		switch {
-		case akfile == "-":
-			data, err = ioutil.ReadAll(os.Stdin)
-		case strings.HasPrefix(akfile, "http://"):
-			log.Fatal("HTTP import URLs are vulnerable to MitM attacks. Use HTTPS instead.")
-		case strings.HasPrefix(akfile, "https://"):
-			data, err = download(akfile)
-		default:
-			data, err = ioutil.ReadFile(akfile)
-		}
-
-		if err != nil {
-			log.Fatalf("could not read accesskey file: %s", err)
-		}
-
-		ak := &accesskey.T{}
-		err = json.Unmarshal(data, &ak)
-
-		if err != nil {
-			log.Fatal("could not unmarshal accesskey file: ", err)
-		}
-
-		switch {
-		case ak == nil,
-			ak.Version == nil,
-			ak.Contract == nil,
-			ak.Pofs == nil,
-			ak.Contract.Endpoint == nil,
-			ak.Contract.PublicKey == nil:
-			log.Fatal("malformed accesskey file")
-		}
-
-		if ak.Version.Minor != accesskey.VERSION.Minor {
-			log.Fatalf(
-				"incompatible accesskey version: %s, expected 0.%d.x",
-				ak.Version,
-				accesskey.VERSION.Minor,
-			)
-		}
-
-		sc0 := clientlib.ContractURL(fm)
-		if sc0 != nil && *sc0 != *ak.Contract.Endpoint {
-			log.Fatalf(
-				"you are trying to import accesskeys for a contract %s different from the currently defined %s; please set up a separate wireleap directory for your %s needs and import %s accesskeys there",
-				ak.Contract.Endpoint,
-				sc0,
-				ak.Contract.Endpoint,
-				akfile,
-			)
-		}
-
-		cl := client.New(nil, clientcontract.T)
-		ci, d, err := clientlib.GetContractInfo(cl, ak.Contract.Endpoint)
-
-		if err != nil {
-			log.Fatalf(
-				"could not get contract info for %s: %s",
-				ak.Contract.Endpoint, err,
-			)
-		}
-
-		if !bytes.Equal(ak.Contract.PublicKey, ci.Pubkey) {
-			log.Fatalf(
-				"contract public key mismatch; expecting %s from accesskey file, got %s from live contract",
-				ak.Contract.PublicKey,
-				base64.RawURLEncoding.EncodeToString(ci.Pubkey),
-			)
-		}
-
-		if err = clientlib.SaveContractInfo(fm, ci, d); err != nil {
-			log.Fatalf(
-				"could not save contract info for %s: %s",
-				ak.Contract.Endpoint,
-				err,
-			)
-		}
-		sc := ci.Endpoint
-		pofs := []*pof.T{}
-		if err = fm.Get(&pofs, filenames.Pofs); errors.Is(err, io.EOF) || errors.Is(err, os.ErrNotExist) {
-			// this is fine
-			err = nil
-		}
-		if err != nil {
-			log.Fatalf(
-				"could not get previous pofs for %s: %s",
-				sc.String(),
-				err,
-			)
-		}
-
-		for _, p := range ak.Pofs {
-			if p.Expiration <= time.Now().Unix() {
-				log.Printf("skipping expired accesskey %s", p.Digest())
-				continue
+			if err != nil {
+				log.Fatal(err)
 			}
-			dup := false
-			for _, p0 := range pofs {
-				if p0.Digest() == p.Digest() {
-					log.Printf("skipping duplicate accesskey %s", p.Digest())
-					dup = true
-					break
-				}
-			}
-			if !dup {
-				pofs = append(pofs, p)
-			}
-		}
 
-		if err = fm.Set(pofs, filenames.Pofs); err != nil {
-			log.Fatalf(
-				"could not save new pofs for %s: %s",
-				sc.String(), err,
-			)
-		}
-		di, err := consume.DirectoryInfo(cl, sc)
-		if err != nil {
-			log.Fatalf("could not get contract directory info: %s", err)
-		}
-		// maybe there's an upgrade available?
-		var upgradev *semver.Version
-		if di.UpgradeChannels.Client != nil {
-			if v, ok := di.UpgradeChannels.Client[version.Channel]; ok && v.GT(version.VERSION) {
-				upgradev = &v
+			if fs.NArg() != 1 {
+				r.Usage()
+				os.Exit(1)
 			}
-		}
-		var pid int
-		if err = fm.Get(&pid, filenames.Pid); err == nil {
-			if upgradev != nil {
-				log.Printf(
-					"Upgrade available to %s, current version is %s. Please run `wireleap upgrade`.",
-					upgradev, version.VERSION,
+
+			akfile := fs.Arg(0)
+			data := []byte{}
+
+			switch {
+			case akfile == "-":
+				data, err = ioutil.ReadAll(os.Stdin)
+			case strings.HasPrefix(akfile, "http://"):
+				log.Fatal("HTTP import URLs are vulnerable to MitM attacks. Use HTTPS instead.")
+			case strings.HasPrefix(akfile, "https://"):
+				data, err = download(akfile)
+			default:
+				data, err = ioutil.ReadFile(akfile)
+			}
+
+			if err != nil {
+				log.Fatalf("could not read accesskey file: %s", err)
+			}
+
+			ak := &accesskey.T{}
+			err = json.Unmarshal(data, &ak)
+
+			if err != nil {
+				log.Fatal("could not unmarshal accesskey file: ", err)
+			}
+
+			switch {
+			case ak == nil,
+				ak.Version == nil,
+				ak.Contract == nil,
+				ak.Pofs == nil,
+				ak.Contract.Endpoint == nil,
+				ak.Contract.PublicKey == nil:
+				log.Fatal("malformed accesskey file")
+			}
+
+			if ak.Version.Minor != accesskey.VERSION.Minor {
+				log.Fatalf(
+					"incompatible accesskey version: %s, expected 0.%d.x",
+					ak.Version,
+					accesskey.VERSION.Minor,
 				)
 			}
-			process.Reload(pid)
-		}
+
+			sc0 := clientlib.ContractURL(fm)
+			if sc0 != nil && *sc0 != *ak.Contract.Endpoint {
+				log.Fatalf(
+					"you are trying to import accesskeys for a contract %s different from the currently defined %s; please set up a separate wireleap directory for your %s needs and import %s accesskeys there",
+					ak.Contract.Endpoint,
+					sc0,
+					ak.Contract.Endpoint,
+					akfile,
+				)
+			}
+
+			cl := client.New(nil, clientcontract.T)
+			ci, d, err := clientlib.GetContractInfo(cl, ak.Contract.Endpoint)
+
+			if err != nil {
+				log.Fatalf(
+					"could not get contract info for %s: %s",
+					ak.Contract.Endpoint, err,
+				)
+			}
+
+			if !bytes.Equal(ak.Contract.PublicKey, ci.Pubkey) {
+				log.Fatalf(
+					"contract public key mismatch; expecting %s from accesskey file, got %s from live contract",
+					ak.Contract.PublicKey,
+					base64.RawURLEncoding.EncodeToString(ci.Pubkey),
+				)
+			}
+
+			if err = clientlib.SaveContractInfo(fm, ci, d); err != nil {
+				log.Fatalf(
+					"could not save contract info for %s: %s",
+					ak.Contract.Endpoint,
+					err,
+				)
+			}
+			sc := ci.Endpoint
+			pofs := []*pof.T{}
+			if err = fm.Get(&pofs, filenames.Pofs); errors.Is(err, io.EOF) || errors.Is(err, os.ErrNotExist) {
+				// this is fine
+				err = nil
+			}
+			if err != nil {
+				log.Fatalf(
+					"could not get previous pofs for %s: %s",
+					sc.String(),
+					err,
+				)
+			}
+
+			for _, p := range ak.Pofs {
+				if p.Expiration <= time.Now().Unix() {
+					log.Printf("skipping expired accesskey %s", p.Digest())
+					continue
+				}
+				dup := false
+				for _, p0 := range pofs {
+					if p0.Digest() == p.Digest() {
+						log.Printf("skipping duplicate accesskey %s", p.Digest())
+						dup = true
+						break
+					}
+				}
+				if !dup {
+					pofs = append(pofs, p)
+				}
+			}
+
+			if err = fm.Set(pofs, filenames.Pofs); err != nil {
+				log.Fatalf(
+					"could not save new pofs for %s: %s",
+					sc.String(), err,
+				)
+			}
+			di, err := consume.DirectoryInfo(cl, sc)
+			if err != nil {
+				log.Fatalf("could not get contract directory info: %s", err)
+			}
+			// maybe there's an upgrade available?
+			var upgradev *semver.Version
+			if di.UpgradeChannels.Client != nil {
+				if v, ok := di.UpgradeChannels.Client[version.Channel]; ok && v.GT(version.VERSION) {
+					upgradev = &v
+				}
+			}
+			var pid int
+			if err = fm.Get(&pid, filenames.Pid); err == nil {
+				if upgradev != nil {
+					log.Printf(
+						"Upgrade available to %s, current version is %s. Please run `wireleap upgrade`.",
+						upgradev, version.VERSION,
+					)
+				}
+				process.Reload(pid)
+			}
+		*/
 	}
 	r.SetMinimalUsage("FILE|URL")
 	return r
