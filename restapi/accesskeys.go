d67c46afd04f7490cb5edef1a4607cd85017c488
Anton Pyrogovskyi <anton@wireleap.com>
2022-06-01 19:45:24 +0200

    factor out accesskeys handler

---
+++ restapi/accesskeys.go
@@ -0,0 +1,47 @@
+package restapi
+
+import (
+	"time"
+
+	"github.com/wireleap/common/api/texturl"
+)
+
+type AccesskeyImportRequest struct {
+	URL *texturl.URL `json:"url"`
+}
+
+type accesskeyReply struct {
+	Contract   *texturl.URL `json:"contract"`
+	Duration   int64        `json:"duration"`
+	State      string       `json:"state"`
+	Expiration int64        `json:"expiration"`
+}
+
+func (t *T) newAccesskeysReply() (rs []*accesskeyReply) {
+	ci, err := t.br.ContractInfo()
+	if err != nil {
+		return
+	}
+	// get contract info
+	if sk := t.br.AKM.CurrentSK(); sk != nil {
+		state := "active"
+		if sk.IsExpiredAt(time.Now().Unix()) {
+			state = "expired"
+		}
+		rs = append(rs, &accesskeyReply{
+			Contract:   ci.Endpoint,
+			Duration:   int64(ci.Servicekey.Duration),
+			State:      state,
+			Expiration: sk.Contract.SettlementOpen,
+		})
+	}
+	for _, p := range t.br.AKM.CurrentPofs() {
+		rs = append(rs, &accesskeyReply{
+			Contract:   ci.Endpoint,
+			Duration:   int64(ci.Servicekey.Duration),
+			State:      "inactive",
+			Expiration: p.Expiration,
+		})
+	}
+	return
+}
