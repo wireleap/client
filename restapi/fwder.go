ca6a436985bd9d61147c2a41b557cd28e933ee7e
Anton Pyrogovskyi <anton@wireleap.com>
2022-07-10 17:12:52 +0200

    restapi: mock getBinaryState() for GOOS=windows

--- restapi/fwder.go
+++ restapi/fwder.go
@@ -12,7 +12,6 @@ import (
 	"os"
 	"os/exec"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/wireleap/common/api/client"
@@ -49,24 +48,6 @@ type FwderState struct {
 }
 
 func boolptr(x bool) *bool { return &x }
-
-func (t *T) getBinaryState(bin string) (st binaryState) {
-	fi, err := os.Stat(t.br.Fd.Path(bin))
-	if err != nil {
-		return
-	}
-	st.Exists = true
-	st.ChmodX = fi.Mode()&0100 != 0
-	if bin == fwderPrefix+"tun" {
-		if stat, ok := fi.Sys().(*syscall.Stat_t); ok && stat.Uid == 0 {
-			st.Chown0 = boolptr(true)
-		} else {
-			st.Chown0 = boolptr(false)
-		}
-		st.ChmodUS = boolptr(fi.Mode()&os.ModeSetuid != 0)
-	}
-	return
-}
 
 func (t *T) registerForwarder(name string) {
 	var (
