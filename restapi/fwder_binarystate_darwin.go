665a7a10425c18f4e62ba672a59e0fc5e45488e0
Anton Pyrogovskyi <anton@wireleap.com>
2022-07-10 17:12:52 +0200

    restapi: mock getBinaryState() for GOOS=windows

---
+++ restapi/fwder_binarystate_darwin.go
@@ -0,0 +1,24 @@
+package restapi
+
+import (
+	"os"
+	"syscall"
+)
+
+func (t *T) getBinaryState(bin string) (st binaryState) {
+	fi, err := os.Stat(t.br.Fd.Path(bin))
+	if err != nil {
+		return
+	}
+	st.Exists = true
+	st.ChmodX = fi.Mode()&0100 != 0
+	if bin == fwderPrefix+"tun" {
+		if stat, ok := fi.Sys().(*syscall.Stat_t); ok && stat.Uid == 0 {
+			st.Chown0 = boolptr(true)
+		} else {
+			st.Chown0 = boolptr(false)
+		}
+		st.ChmodUS = boolptr(fi.Mode()&os.ModeSetuid != 0)
+	}
+	return
+}
---
+++ restapi/fwder_binarystate_windows.go
@@ -0,0 +1,17 @@
+package restapi
+
+import "os"
+
+func (t *T) getBinaryState(bin string) (st binaryState) {
+	if _, err := os.Stat(t.br.Fd.Path(bin)); err != nil {
+		return
+	}
+	st.Exists = true
+	// NOTE not implemented on windows, just pretend everything is ok
+	st.ChmodX = true
+	if bin == fwderPrefix+"tun" {
+		st.Chown0 = boolptr(true)
+		st.ChmodUS = boolptr(true)
+	}
+	return
+}
