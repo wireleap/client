2e40685d446d76a9c8eb5b0e552fc760605fb3fd
Anton Pyrogovskyi <anton@wireleap.com>
2022-08-10 18:39:48 +0200

    contrib: change - to + in gitversion.sh

--- contrib/gitversion.sh
+++ contrib/gitversion.sh
@@ -6,7 +6,7 @@ fatal() { echo "FATAL [$SCRIPT_NAME]: $*" 1>&2; exit 1; }
 command -v git >/dev/null || fatal "git not installed"
 
 if git describe > /dev/null 2>&1; then
-    git describe | sed 's/^v//'
+    git describe | sed 's/^v//; s/-/+/'
 else
-    echo "0.0.0-$(git describe --always)"
+    echo "0.0.0+$(git describe --always)"
 fi
