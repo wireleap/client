7a75b78159d134e061b23d55d50d80434a69d723
Anton Pyrogovskyi <anton@wireleap.com>
2022-09-04 20:38:37 +0200

    restapi: make upgrade status nullable

--- restapi/status.go
+++ restapi/status.go
@@ -1,11 +1,11 @@
 package restapi
 
 type StatusReply struct {
-	Home    string        `json:"home"`
-	Pid     int           `json:"pid"`
-	State   string        `json:"state"`
-	Broker  StatusBroker  `json:"broker"`
-	Upgrade StatusUpgrade `json:"upgrade"`
+	Home    string         `json:"home"`
+	Pid     int            `json:"pid"`
+	State   string         `json:"state"`
+	Broker  StatusBroker   `json:"broker"`
+	Upgrade *StatusUpgrade `json:"upgrade,omitempty"`
 }
 
 type StatusBroker struct {
